4fb067ea4138281ea15e02d9aa85db3df5f68c7c
marjorie echu <marjbogo@gmail.com>
2025-03-09 18:55:33 +0100

    deploying P&G with DC

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dockercomposeP-G
 Deploying Prometheus STAK on docker compose application
+
+# docker-compose file
+We wrote a docker-compose.yaml file to run the two containers 
+-the frontend and 
+-the backend and very importantly 
+-to persist volume
+
---
+++ lab.sh
@@ -0,0 +1,11 @@
+docker-compose up -d    #runs the container
+
+docker-compose logs -f  #checks logs
+
+docker-compose down     #stops containers without loosing data
+
+docker-compose down -v  #deletes everytthing including data
+
+docker-compose ls
+
+docker stop container
