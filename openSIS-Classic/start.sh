f24c76652cfb355d0a723ce42152531dbed49521
marjorie echu <marjbogo@gmail.com>
2025-03-21 13:25:00 +0100

    rev

--- openSIS-Classic/start.sh
+++ openSIS-Classic/start.sh
@@ -1,6 +1,20 @@
-#!/bin/bash
+# #!/bin/bash
 
-service apache2 start
+# service apache2 start
 
-#keep the container running
-tail -f /dev/null
+# #keep the container running
+# tail -f /dev/null
+
+
+
+# #!/bin/bash
+# set -e  # Exit on errors
+
+# # Start Apache in the foreground
+# service apache2 start
+
+# # Start the Grafana dashboard manager in the background
+# python3 /opt/grafana_dashboard_manager.py &
+
+# # Keep Apache and Python script running in the foreground
+# exec apachectl -D FOREGROUND
